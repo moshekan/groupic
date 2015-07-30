2fd8f3085fd10f0ce41a8b6fe48d62ca8d774b10
brit13-meet <brit13@meet.mit.edu>
2015-07-30 18:36:24 +0300

    views modified, ajax endpoints

--- groupic/events/urls.py
+++ groupic/events/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'events', views.events, name='events'),
+	url(r'event_detail', views.event_detail, name='event_detail'),
 ]
--- groupic/events/views.py
+++ groupic/events/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from annoying.decorators import ajax_request, render_to
+from get_instagram_data import FILENAME
+import json
+import os.path as path
+FILENAME=path.join(path.dirname(path.realpath(__file__)), FILENAME)
 
-# Create your views here.
+@render_to('events.html')
 def index(request):
 	context = {"events" : [{"id":1, "eventName":"Maor"},{"id":2,"eventName":"Nadav"},{"id":3,"eventName":"Tamar"}]}
-	return render(request, 'events.html', context)
+	return context
+
+
+@ajax_request
+def events(request):
+	return get_json_data(FILENAME)
+
+@ajax_request
+def event_detail(request):
+	event_name=request.GET.get('event_name')
+	data = get_json_data(FILENAME)
+	if event_name is None:
+		return data.values()[0]
+	return data[event_name]
+	
+
+def get_json_data(filename):
+	with open(filename) as f:
+		return json.loads(f.read())
+
+

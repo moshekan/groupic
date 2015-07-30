822d6e04913483cf4b58cd7e7c4c6f7868f41b9f
Maor Kern <maorkern@nwcse-guset-0852.cs.huji.ac.il>
2015-07-30 14:16:03 +0300

    Frontend code

---
+++ groupic/groupic/static/js/events.js
@@ -0,0 +1,32 @@
+function loadEvent(id){
+	//Put loading symbol
+
+	//Make ajax request
+	.ajax( "/ASKJOSH" )
+  .done(function(eventsArray) {
+  	//When ajax request ends- loadPhotos(photos)
+    loadPhotos(eventsArray);
+  })
+  .fail(function() {
+    console.log("Error");
+  });
+	
+
+}
+
+function loadPhotos(photos){
+	//Throw away the events except the one we loaded - (keep them)
+
+	//create elements for images underneat main
+
+	//animate these images
+
+	//Back button element unhide
+
+}
+
+function goBack(){
+	//Make photosgo back into event
+
+	//Load back events
+}

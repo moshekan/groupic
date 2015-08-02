086d6feac9ef074055c82b9b22b6bbf01aa729c9
Your Name <you@example.com>
2015-08-02 17:58:27 +0300

    landing page

--- groupic/events/urls.py
+++ groupic/events/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	url(r'^landing$', views.landing, name='landing'),
+    	url(r'^$', views.index, name='index'),
 	url(r'events', views.events, name='events'),
 	url(r'event_detail', views.event_detail, name='event_detail'),
 ]

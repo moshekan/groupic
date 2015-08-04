3b2948831960b2158d4001dd92c0505f1d6a34a3
Your Name <you@example.com>
2015-08-04 11:13:17 +0300

    none

--- groupic/events/models.py
+++ groupic/events/models.py
@@ -1,12 +1,4 @@
 from django.db import models
-
-class Event(models.Model):
-	event_id = models.AutoField(primary_key=True)
-    event_name = models.CharField(max_length=50)
-    event_type = models.IntegerField(max_length=1)
-    users = models.ManyToMantField(Users)
-    media = models.ManyToMantField(Media)
-    admins = models.ManyToMantField(Admins)
-    location = 10
-
 # Create your models here.
+
+
